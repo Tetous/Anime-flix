3a824d3cdb4088034815bab7c87ee0477ba97c3d
Richard Kopelow <richardkopelow@gmail.com>
2014-08-26 00:08:01 -0400

    Implemented resizing of the IconView. Icons transition to their new locations and the spacing is dynamic so that there are no uneven margins.

--- app/src/NillaIconView.js
+++ app/src/NillaIconView.js
@@ -3,9 +3,12 @@
  * Copyright: Copyright 2014 Richard Kopelow
  */
 
-define(function(require, exports, module) {
+define(function (require, exports, module)
+{
+    var Engine = require('famous/core/Engine');
 	var View = require('famous/core/View');
 	var Transform = require('famous/core/Transform');
+	var Easing = require('famous/transitions/Easing');
 	var StateModifier= require('famous/modifiers/StateModifier');
 	var Surface=require('famous/core/Surface');
 	var ContainerSurface=require('famous/surfaces/ContainerSurface');
@@ -13,7 +16,9 @@ define(function(require, exports, module) {
 	var Scrollview=require('famous/views/Scrollview');
 	var Icon=require('NillaIcon');
 
-	function createNillaIconView(){
+	function createNillaIconView() {
+	    var iconStateMods = [];
+
 		var view=new View();
 		var container=new ContainerSurface({
 			//size:window.mainContext.getSize(),
@@ -28,9 +33,6 @@ define(function(require, exports, module) {
 		  });
 
 		var iconView=new View();
-		iconView.on('click',function(){
-			console.log('iconView Clicked');
-		});
 		container.pipe(scroll);
 
 		scroll.sequenceFrom([iconView]);
@@ -39,28 +41,47 @@ define(function(require, exports, module) {
 
 		view.populate=function populate(shows)
 		{
-			var iconBufferX=50;
-
-			var iconWidth=150;
-			var iconHeight=iconWidth*1.555;
-
-			var imagesPerRow=Math.floor((window.mainContext.getSize()[0]-iconBufferX)/(iconWidth+iconBufferX));
-			iconView.setOptions({size:[undefined,100+Math.floor(shows.length/imagesPerRow+0.95)*(iconHeight+100)]})
-
 			for (var i = 0; i < shows.length; i++) {
 				var showData=shows[i];
 
-				var iconTransform=new StateModifier({
-					transform:Transform.translate(i%imagesPerRow*(iconWidth+iconBufferX)+iconBufferX,100+Math.floor(i/imagesPerRow)*(iconHeight+100),window.showSelectorZ+10)
-				});
-				var icon=Icon(showData);
-				icon.on('click',function(icon){
-					view._eventOutput.emit('iconClick', icon.data);
+				var iconTransform = new StateModifier({});
+				iconStateMods.push(iconTransform);
+				var icon = Icon(showData);
+				icon.on('click', function (icon)
+				{
+				    view._eventOutput.emit('iconClick', icon.data);
 				});
 
 				iconView.add(iconTransform).add(icon);
 			};
+			positionIcons(0);
 		}
+
+		function positionIcons(duration)
+		{
+		    var windowSize = window.mainContext.getSize();
+
+		    var minIconBufferX = 50;
+		    var iconWidth = 150;
+		    var iconHeight = iconWidth * 1.555;
+		    var imagesPerRow = Math.floor((windowSize[0] - minIconBufferX) / (iconWidth + minIconBufferX));
+
+		    var iconBufferX = (windowSize[0]-(minIconBufferX + (minIconBufferX + iconWidth) * imagesPerRow))/(imagesPerRow+1)+minIconBufferX;
+
+		    iconView.setOptions({ size: [undefined, 100 + Math.floor(iconStateMods.length / imagesPerRow + 0.95) * (iconHeight + 100)] });
+
+		    for (var i = 0; i < iconStateMods.length; i++)
+		    {
+		        iconStateMods[i].halt();
+		        iconStateMods[i].setTransform(Transform.translate(i % imagesPerRow * (iconWidth + iconBufferX) + iconBufferX, 100 + Math.floor(i / imagesPerRow) * (iconHeight + 100), window.showSelectorZ + 10), { duration: duration, curve: Easing.outCubic });
+		    }
+		}
+
+		Engine.on('resize', function ()
+		{
+		    positionIcons(1000);
+		});
+
 		return view;
 	}
 	module.exports=createNillaIconView;
